a8185aa61839dac03eff39cac231020af265c16f
Fernando da Costa Kudrna <m78181@ACAD.UNISC.BR>
2019-08-13 20:18:31 -0300

    Initial commit

---
+++ app/src/main/java/com/m78181/pdm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.m78181.pdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //essa activity está associada a activity_main
+    }
+}
