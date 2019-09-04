7f12ec469c3c92a2bfe672f8b3fa491f2f51d206
blzfe <fernandokudrna@mx2.unisc.br>
2019-09-04 14:51:51 -0300

    Exercício Aula 3 completo Outros erros resolvidos

--- app/src/main/java/com/m78181/pdm/Main3Activity.java
+++ app/src/main/java/com/m78181/pdm/Main3Activity.java
@@ -1,7 +1,9 @@
 package com.m78181.pdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -10,4 +12,26 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+
+    protected void onClickq1(View view){
+        Intent i = new Intent(this, Main3p1Activity.class);
+        startActivity(i);
+    }
+
+    protected void onClickq2(){
+        Intent i = new Intent(this, Main3p2Activity.class);
+        startActivity(i);
+    }
+
+    protected void onClickq3(){
+        Intent i = new Intent(this, Main3p3Activity.class);
+        startActivity(i);
+    }
+
+    protected void onClickq4(){
+        Intent i = new Intent(this, Main3p4Activity.class);
+        startActivity(i);
+    }
+
+
 }
---
+++ app/src/main/java/com/m78181/pdm/Main3p2Activity.java
@@ -0,0 +1,13 @@
+package com.m78181.pdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3p2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3p2);
+    }
+}
